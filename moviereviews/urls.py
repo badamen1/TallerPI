28cc9b7afb688a400e873599e23b314c9f9e146d
Bayron Mena <bayronmenar@gmail.com>
2024-07-31 23:08:03 -0500

    visualizacion de películas con estilo

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,4 @@ urlpatterns = [
 
 
 ]
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
